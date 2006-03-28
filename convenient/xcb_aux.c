f88a511cdb5e931cecca4831124f94a713e08f0b
Ian Osgood <iano@quirkster.com>
2006-03-27 18:30:34 -0800

    Add some convenience functions for avoiding value lists. These are less error-prone, and should be more familiar to Xlib programmers.

--- convenient/xcb_aux.c
+++ convenient/xcb_aux.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include <X11/XCB/xcb.h>
+#include "xcb_aux.h"
 
 /* Connection related functions */
 
@@ -60,4 +61,97 @@ XCBAuxGetVisualtype (XCBConnection *c,
 	 return iter.data;
 
    return NULL;
+}
+
+/* structs instead of value lists */
+/* TODO: generate the struct types and functions from protocol masks and descriptions */
+
+/* This generic implementation of pack_list depends on:
+   a) structs packed to CARD32 size
+   b) structs consist of just CARD32/INT32 fields in the same order as bitmask
+*/
+
+static void
+pack_list( CARD32 mask, const CARD32 *src, CARD32 *dest )
+{
+	for ( ; mask; mask >>= 1, src++)
+		if (mask & 1)
+			*dest++ = *src;
+}
+
+XCBVoidCookie
+XCBAuxCreateWindow (XCBConnection *c,
+                 CARD8          depth,
+                 XCBWINDOW      wid,
+                 XCBWINDOW      parent,
+                 INT16          x,
+                 INT16          y,
+                 CARD16         width,
+                 CARD16         height,
+                 CARD16         border_width,
+                 CARD16         _class,
+                 XCBVISUALID    visual,
+                 CARD32         mask,
+                 const XCBParamsCW *params)
+{
+	CARD32 value_list[16];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBCreateWindow(c, depth, wid, parent,
+		x, y, width, height, border_width,
+		_class, visual, mask, value_list);
+}
+
+XCBVoidCookie
+XCBAuxChangeWindowAttributes (XCBConnection *c,
+                           XCBWINDOW      window,
+                           CARD32         mask,
+                           const XCBParamsCW *params)
+{
+	CARD32 value_list[16];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBChangeWindowAttributes( c, window, mask, value_list );
+}
+
+XCBVoidCookie
+XCBAuxConfigureWindow (XCBConnection *c,
+                    XCBWINDOW      window,
+                    CARD16         mask,
+                    const XCBParamsConfigureWindow *params)
+{
+	CARD32 value_list[8];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBConfigureWindow( c, window, mask, value_list );
+}
+
+XCBVoidCookie
+XCBAuxCreateGC (XCBConnection *c,
+             XCBGCONTEXT    gid,
+             XCBDRAWABLE    drawable,
+             CARD32         mask,
+             const XCBParamsGC *params)
+{
+	CARD32 value_list[32];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBCreateGC( c, gid, drawable, mask, value_list );
+}
+
+XCBVoidCookie
+XCBAuxChangeGC (XCBConnection *c,
+             XCBGCONTEXT    gc,
+             CARD32         mask,
+             const XCBParamsGC *params)
+{
+	CARD32 value_list[32];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBChangeGC( c, gc, mask, value_list );
+}
+
+XCBVoidCookie
+XCBAuxChangeKeyboardControl (XCBConnection *c,
+                          CARD32         mask,
+                          const XCBParamsKeyboard *params)
+{
+	CARD32 value_list[16];
+	pack_list(mask, (const CARD32 *)params, value_list);
+	return XCBChangeKeyboardControl( c, mask, value_list );
 }
--- convenient/xcb_aux.h
+++ convenient/xcb_aux.h
@@ -13,4 +13,116 @@ XCBVISUALTYPE *XCBAuxGetVisualtype (XCBConnection *c,
 				    XCBVISUALID    vid);
 
 
+/* less error prone to use structs instead of value lists */
+
+typedef struct {
+    CARD32 back_pixmap;
+    CARD32 back_pixel;
+    CARD32 border_pixmap;
+    CARD32 border_pixel;
+    CARD32 bit_gravity;
+    CARD32 win_gravity;
+    CARD32 backing_store;
+    CARD32 backing_planes;
+    CARD32 backing_pixel;
+    CARD32 override_redirect;
+    CARD32 save_under;
+    CARD32 event_mask;
+    CARD32 dont_propagate;
+    CARD32 colormap;
+    CARD32 cursor;
+} XCBParamsCW;
+
+XCBVoidCookie
+XCBAuxCreateWindow (XCBConnection *c,
+                 CARD8          depth,
+                 XCBWINDOW      wid,
+                 XCBWINDOW      parent,
+                 INT16          x,
+                 INT16          y,
+                 CARD16         width,
+                 CARD16         height,
+                 CARD16         border_width,
+                 CARD16         _class,
+                 XCBVISUALID    visual,
+                 CARD32         mask,
+                 const XCBParamsCW *params);
+
+XCBVoidCookie
+XCBAuxChangeWindowAttributes (XCBConnection *c,
+                           XCBWINDOW      window,
+                           CARD32         mask,
+                           const XCBParamsCW *params);
+
+typedef struct {
+    INT32  x;
+    INT32  y;
+    CARD32 width;
+    CARD32 height;
+    CARD32 border_width;
+    CARD32 sibling;
+    CARD32 stack_mode;
+} XCBParamsConfigureWindow;
+
+XCBVoidCookie
+XCBAuxConfigureWindow (XCBConnection *c,
+                    XCBWINDOW      window,
+                    CARD16         mask,
+                    const XCBParamsConfigureWindow *params);
+
+typedef struct {
+    CARD32 function;
+    CARD32 plane_mask;
+    CARD32 foreground;
+    CARD32 background;
+    CARD32 line_width;
+    CARD32 line_style;
+    CARD32 cap_style;
+    CARD32 join_style;
+    CARD32 fill_style;
+    CARD32 fill_rule;
+    CARD32 tile;
+    CARD32 stipple;
+    CARD32 tile_stipple_originX;
+    CARD32 tile_stipple_originY;
+    CARD32 font;
+    CARD32 subwindow_mode;
+    CARD32 graphics_exposures;
+    CARD32 clip_originX;
+    CARD32 clip_originY;
+    CARD32 mask;
+    CARD32 dash_offset;
+    CARD32 dash_list;
+    CARD32 arc_mode;
+} XCBParamsGC;
+
+XCBVoidCookie
+XCBAuxCreateGC (XCBConnection *c,
+             XCBGCONTEXT    cid,
+             XCBDRAWABLE    drawable,
+             CARD32         mask,
+             const XCBParamsGC *params);
+
+XCBVoidCookie
+XCBAuxChangeGC (XCBConnection *c,
+             XCBGCONTEXT    gc,
+             CARD32         mask,
+             const XCBParamsGC *params);
+
+typedef struct {
+    CARD32 key_click_percent;
+    CARD32 bell_percent;
+    CARD32 bell_pitch;
+    CARD32 bell_duration;
+    CARD32 led;
+    CARD32 led_mode;
+    CARD32 key;
+    CARD32 auto_repeat_mode;
+} XCBParamsKeyboard;
+
+XCBVoidCookie
+XCBAuxChangeKeyboardControl (XCBConnection *c,
+                          CARD32         mask,
+                          const XCBParamsKeyboard *params);
+
 #endif /* __XCB_CONVENIENT_H__ */
